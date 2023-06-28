c7bf2f0e328081dd522c5866d61652a1561061b8
Virnandaa <ilham.virnanda1@gmail.com>
2023-06-28 17:07:29 +0700

    first commit

---
+++ Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lagu.Startup))]
+namespace lagu
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+
+            
+        }
+    }
+}
